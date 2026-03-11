759ee3bb264e50094e2d226d70b180fd872817cf
HOANG <hoang261104@gmail.com>
2026-03-11 21:09:29 +0700

    fist

---
+++ src/main/java/com/example/be_microservices/BeMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMicroservicesApplication.class, args);
+	}
+
+}
